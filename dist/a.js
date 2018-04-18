12dcd175b111cd74cbb41d9abb4cb93b38633f61
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-18 17:46:21 +0700

    tsc

---
+++ dist/a.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log(1234);
+function concatString(a, b) {
+    return a + b;
+}
+var str = concatString('x', 'y');
+console.log(str);
---
+++ src/a.ts
@@ -0,0 +1,8 @@
+console.log(1234);
+
+function concatString(a: string, b: string) {
+    return a + b;
+}
+
+const str = concatString('x', 'y');
+console.log(str);
